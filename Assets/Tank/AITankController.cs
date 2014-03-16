bd881f55e08970e0ab6fe5bc893c9c361c4e667e
Sam Osterkil <pisketch@gmail.com>
2014-03-16 14:58:50 -0400

    Target acquisition logic, probably working

--- Assets/Tank/AITankController.cs
+++ Assets/Tank/AITankController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AITankController : MonoBehaviour {
 
@@ -7,6 +8,8 @@ public class AITankController : MonoBehaviour {
     public enum States {Sleeping, Chasing, Searching, Fleeing}
     public States currentState;
     public int health;
+    private Vector3 targetLoc; // The position the tank is moving towards
+    private List<GameObject> enemyList; //The list of things this tank considers an enemy
 
 	// Use this for initialization
 	void Start () {
@@ -14,9 +17,13 @@ public class AITankController : MonoBehaviour {
         health = 100;
         if(Random.Range(0, 1) == 0){
             isFriendly = false;
+            // TODO: Replace this with GameManager list checking
+            gameObject.tag = "enemy";
         }
         else{
             isFriendly = true;
+            // TODO: Replace this with GameManager list checking
+            gameObject.tag = "friendly";
         }
 	}
 
@@ -24,4 +31,18 @@ public class AITankController : MonoBehaviour {
 	void Update () {
 
 	}
+
+    bool getTarget(){
+        foreach (GameObject enemy in enemyList){
+            RaycastHit hit;
+            Vector3 toEnemy = transform.position - enemy.transform.position;
+            if (Physics.Linecast(transform.position + new Vector3(0, 1, 0), toEnemy, out hit)){
+                if (hit.collider.gameObject == enemy){
+                    targetLoc = enemy.transform.position;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
