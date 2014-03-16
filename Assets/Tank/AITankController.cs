35abef01dee089e1dd34e6f8b0ad93039b6dad5c
Sam Osterkil <pisketch@gmail.com>
2014-03-16 13:37:12 -0400

    Initial Commit

---
+++ Assets/Tank/AITankController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AITankController : MonoBehaviour {
+
+    public bool isFriendly;
+    public enum States {Sleeping, Chasing, Searching, Fleeing}
+    public States currentState;
+    public int health;
+
+	// Use this for initialization
+	void Start () {
+        currentState = States.Sleeping;
+        health = 100;
+        if(Random.Range(0, 1) == 0){
+            isFriendly = false;
+        }
+        else{
+            isFriendly = true;
+        }
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
