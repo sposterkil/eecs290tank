77d4fad800ca1d4e69b74c07d6b914e172e5d3fe
Kaan Atesoglu <aka43@case.edu>
2014-03-18 16:02:45 -0400

    Added the beginnings of spawning
    press f5 to spawn some tanks in the air

---
+++ Assets/SpawnScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+	public Transform spawn1; // places that are set
+	public Transform tank;
+
+
+	// Use this for initialization
+	void Start () {
+
+	//	StartSpawn ();// start the initial 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// for testing purposes press f5
+		if (Input.GetKeyDown ("f5")) {
+						Spawn ("enemy");
+				}
+	
+	}
+
+
+	/*
+	 * Handles the tank spawning
+	 */
+	void Spawn(string role) {
+		Object ai;
+		if (role.Equals ("enemy")) {
+						Debug.Log ("enemy spawned"); // 
+			ai = Instantiate(tank, spawn1.position, Quaternion.identity);
+
+		} 
+		else if (role.Equals ("ally")) {
+						Debug.Log ("ally spawned");
+
+		} 
+		else { // not a valid spawn
+						Debug.Log ("invalid spawn");
+		}
+		} 
+}
