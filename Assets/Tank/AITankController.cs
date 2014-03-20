fce6b07540d99e254f6f7c3776b9d722bf1e473b
Sam Osterkil <pisketch@gmail.com>
2014-03-20 15:37:59 -0400

    AIController should (in theory) move towards enemy tanks

--- Assets/Tank/AITankController.cs
+++ Assets/Tank/AITankController.cs
@@ -14,6 +14,7 @@ public class AITankController : MonoBehaviour {
 
     private int maxSpeed = 10;
     private int accel = 2;
+
     private Vector3 currentVel = new Vector3(0, 0, 0);
     private Vector3 currentAccel = new Vector3(0, 0, 0);
 
@@ -23,45 +24,54 @@ public class AITankController : MonoBehaviour {
         health = 100;
         if(Random.Range(0, 1) == 0){
             isFriendly = false;
-            // TODO: Replace this with GameManager list checking
             gameObject.tag = "enemy";
+            TankManager.Instance.EnemyTanks.Add(gameObject);
+            enemyList = TankManager.Instance.FriendlyTanks;
+
         }
         else{
             isFriendly = true;
-            // TODO: Replace this with GameManager list checking
+            TankManager.Instance.FriendlyTanks.Add(gameObject);
+            enemyList = TankManager.Instance.EnemyTanks;
             gameObject.tag = "friendly";
         }
 	}
 
 	// Update is called once per frame
 	void Update () {
-        getTarget();
+        targetLoc = getTarget();
         if (targetLoc != null){
+            currentState = States.Chasing;
             setVelTowards(targetLoc);
         }
-        transform.position = transform.position + currentVel;
+
 	}
 
-    void setVelTowards(Vector3 goal){
+    Vector3 setVelTowards(Vector3 goal){
         Vector3 goalVelocity = Vector3.Normalize(goal - transform.position) * maxSpeed;
         Vector3 steerVelocity = Vector3.Normalize(goalVelocity - currentVel) * maxSpeed;
-        currentVel = steerVelocity;
+        return steerVelocity;
     }
 
-    void getTarget(){
-        List<GameObject> seenEnemies = new List<GameObject>();
+    Vector3 getTarget(){
+        Dictionary<float, GameObject> seenEnemies = new Dictionary<float, GameObject>();
         foreach (GameObject enemy in enemyList){
             RaycastHit hit;
             Vector3 toEnemy = transform.position - enemy.transform.position;
             if (Physics.Linecast(transform.position + new Vector3(0, 1, 0), toEnemy, out hit)){
                 if (hit.collider.gameObject == enemy){
-                    seenEnemies.Add(enemy);
+                    seenEnemies.Add(Vector3.Magnitude(transform.position - enemy.transform.position), enemy);
                 }
             }
         }
         if (seenEnemies.Count > 0){
-            // seenEnemies = seenEnemies.OrderBy<GameObject>(Vector3.magnitude(enemy => transform.position - enemy.transform.position));
-            // Sorry for breakin' shit guys!
+            float minKey = seenEnemies.Keys.Min();
+            GameObject closestTank = null;
+            seenEnemies.TryGetValue(minKey, out closestTank);
+            return closestTank.transform.position;
+        }
+        else{
+            return transform.position;
         }
     }
 }
