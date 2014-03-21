ad14b564aa43aedff72860a9ce5e33f6ce292810
Kaan Atesoglu <aka43@case.edu>
2014-03-21 03:50:20 -0400

    Finished all 9 spawn places,
    The AI tank is disabled, until spawned then they are activated.

--- Assets/SpawnScript.cs
+++ Assets/SpawnScript.cs
@@ -6,6 +6,12 @@ public class SpawnScript : MonoBehaviour {
 	public Transform spawn1; // places that are set
 	public Transform spawn2;
 	public Transform spawn3;
+	public Transform spawn4;
+	public Transform spawn5;
+	public Transform spawn6;
+	public Transform spawn7;
+	public Transform spawn8;
+	public Transform spawn9;
 	public Transform tank;
 
 
@@ -31,31 +37,65 @@ public class SpawnScript : MonoBehaviour {
 	 * Random spawn at one of the places
 	 */
 	void Spawn(string role) {
-		Spawn (Random.Range (1, 4), role);
+		Spawn (Random.Range (1, 10), role);
 	}
 
 	/*
 	 * Handles the tank spawning
 	 */
 	void Spawn(int spawn, string role) {
-		Object ai;
+		GameObject ai;
+		Transform t;
 		if (role.Equals ("enemy")) {
 						Debug.Log ("enemy spawned"); // 
 			switch(spawn) {
-				case 1:			
-				ai = Instantiate(tank, spawn1.position, Quaternion.identity);
-				//ai.GetChild("turret").GetChild("cannon").GetChild("TPCamera").enable = false;
-				break;
+				case 1:
+					t = Instantiate(tank, spawn1.position, Quaternion.identity) as Transform;
+					ai = t.gameObject;
+					ai.SetActive(true);
+					break;
 				case 2:
-				ai = Instantiate(tank, spawn2.position, Quaternion.identity);
-				//ai.turret.cannon.TPCamera.enable = false;
-				break;
+					t = Instantiate(tank, spawn2.position, Quaternion.identity) as Transform;
+					ai = t.gameObject;
+					ai.SetActive(true);
+					break;
 				case 3:
-				ai = Instantiate(tank, spawn3.position, Quaternion.identity);
-				//ai.turret.cannon.TPCamera.enable = false;
+					t = Instantiate(tank, spawn3.position, Quaternion.identity) as Transform;
+					ai = t.gameObject;
+					ai.SetActive(true);
+					break;
+				case 4:
+					t = Instantiate(tank, spawn4.position, Quaternion.identity) as Transform;
+					ai = t.gameObject;
+					ai.SetActive(true);
+					break;
+				case 5:
+					t = Instantiate(tank, spawn5.position, Quaternion.identity) as Transform;
+					ai = t.gameObject;
+					ai.SetActive(true);
+					break;
+				case 6:
+					t = Instantiate(tank, spawn6.position, Quaternion.identity) as Transform;
+					ai = t.gameObject;
+					ai.SetActive(true);
+					break;
+				case 7:
+					t = Instantiate(tank, spawn7.position, Quaternion.identity) as Transform;
+					ai = t.gameObject;
+					ai.SetActive(true);
+					break;				
+				case 8:
+					t = Instantiate(tank, spawn8.position, Quaternion.identity) as Transform;
+					ai = t.gameObject;
+					ai.SetActive(true);
 				break;
-			default:
-				Debug.Log ("not a valid spawn");
+				case 9:
+					t = Instantiate(tank, spawn9.position, Quaternion.identity) as Transform;
+					ai = t.gameObject;
+					ai.SetActive(true);
+				break;
+				default:
+					Debug.Log ("not a valid spawn");
 				break;
 			}
 				
