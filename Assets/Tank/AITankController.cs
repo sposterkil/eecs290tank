699452d5d7e3742c1e33cdc3a14527ab6c6b1008
Sam Osterkil <pisketch@gmail.com>
2014-03-24 19:16:44 -0400

    Target selection completed

--- Assets/Tank/AITankController.cs
+++ Assets/Tank/AITankController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 public class AITankController : MonoBehaviour {
 
@@ -11,6 +11,7 @@ public class AITankController : MonoBehaviour {
     public int health;
     private Vector3 targetLoc; // The position the tank is moving towards
     private List<GameObject> enemyList; //The list of things this tank considers an enemy
+    private Transform map;
 
     private int maxSpeed = 10;
     private int accel = 2;
@@ -18,11 +19,12 @@ public class AITankController : MonoBehaviour {
     private Vector3 currentVel = new Vector3(0, 0, 0);
     private Vector3 currentAccel = new Vector3(0, 0, 0);
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        map = GameObject.Find("Map").GetComponent<Transform>();
         currentState = States.Sleeping;
         health = 100;
-        if(Random.Range(0, 1) == 0){
+        if(UnityEngine.Random.Range(0, 1) == 0){
             isFriendly = false;
             gameObject.tag = "enemy";
             TankManager.Instance.EnemyTanks.Add(gameObject);
@@ -47,49 +49,45 @@ public class AITankController : MonoBehaviour {
         foreach (GameObject o in TankManager.Instance.FriendlyTanks){
             Debug.Log(o);
         }
-	}
+    }
 
-	// Update is called once per frame
-	void Update () {
-        targetLoc = getTarget();
+    // Update is called once per frame
+    void Update () {
+        setTarget();
         if (targetLoc != null){
             currentState = States.Chasing;
+        }
+
+        if (currentState == States.Chasing){ // Move towards the target location with proper pursuit behavior
             setVelTowards(targetLoc);
         }
 
+        if (currentState == States.Searching){ // a slow roll foward while looking for things to shoot
 
-	}
+        }
+
+    }
 
     void setVelTowards(Vector3 goal){
         Vector3 goalVelocity = Vector3.Normalize(goal - transform.position) * maxSpeed;
         Vector3 steerVelocity = Vector3.Normalize(goalVelocity - currentVel) * maxSpeed;
-        currentVel = steerVelocity;
+
     }
 
-    Vector3 getTarget(){
+    //Find a target to move towards
+    void setTarget(){
         Dictionary<float, GameObject> seenEnemies = new Dictionary<float, GameObject>();
-        foreach (GameObject enemy in enemyList){
-            RaycastHit hit;
-            Vector3 toEnemy = transform.position - enemy.transform.position;
-            if (Physics.Linecast(transform.position + Vector3.up * 5, toEnemy, out hit)){
-                Debug.DrawLine(transform.position + Vector3.up * 5, hit.point, Color.green);
-                Debug.Log(hit.point);
-                Debug.Log(enemy.transform.position);
-                if (hit.collider.gameObject == enemy){
-                    seenEnemies.Add(Vector3.Magnitude(transform.position - enemy.transform.position), enemy);
-                }
+        foreach (GameObject enemy in enemyList){ // Build a list of enemies seen and the distance to them
+            if (!Physics.Linecast(transform.position + Vector3.up * 3, enemy.transform.position + Vector3.up * 3)){ // If we successfully draw a line from this tank to their enemy...
+                Debug.DrawLine(transform.position + Vector3.up * 3, enemy.transform.position + Vector3.up * 3, Color.green);
+                seenEnemies.Add(Vector3.Magnitude(transform.position - enemy.transform.position), enemy);
             }
         }
-        if (seenEnemies.Count > 0){
+        if (seenEnemies.Count > 0){ //if we've seen some enemies, grab the closest one
             float minKey = seenEnemies.Keys.Min();
             GameObject closestTank = null;
             seenEnemies.TryGetValue(minKey, out closestTank);
-            return closestTank.transform.position;
-            Debug.Log("Moving to target");
-        }
-        else{
-            Debug.Log("No target found");
-            return transform.position;
+            targetLoc = closestTank.transform.position;
         }
     }
 }
