7e1d8a7c361160fb0f39e062c93f07d300b79db1
Sam Osterkil <pisketch@gmail.com>
2014-03-25 00:56:54 -0400

    Increase acceleration a bit

--- Assets/Tank/Panther/Demo Scene/MoveTank.js
+++ Assets/Tank/Panther/Demo Scene/MoveTank.js
@@ -3,7 +3,7 @@
 var leftTrack : MoveTrack;
 var rightTrack : MoveTrack;
 
-var acceleration : float = 2;
+var acceleration : float = 5;
 
 var currentVelocity : float = 0;
 var maxSpeed : float = 10;
@@ -19,12 +19,12 @@ function Start() {
 	// Get Track Controls
 	leftTrack = GameObject.Find(gameObject.name + "/Lefttrack").GetComponent(MoveTrack);
 	rightTrack = GameObject.Find(gameObject.name + "/Righttrack").GetComponent(MoveTrack);
-	
+
 }
 
 
 function Update () {
-	
+
 	if (Input.GetKey (KeyCode.UpArrow)) {
 		// plus speed
 		if (currentVelocity < maxSpeed) {
@@ -35,29 +35,29 @@ function Update () {
 
 	} else if (Input.GetKey (KeyCode.DownArrow)) {
 		// minus speed
-		if (currentVelocity > -maxSpeed) 
+		if (currentVelocity > -maxSpeed)
 			currentVelocity -= acceleration * Time.deltaTime;
 			if (currentVelocity < -maxSpeed)
 				currentVelocity = -maxSpeed;
-		
+
 	} else {
-		// No key input. 
-		if (currentVelocity > 0) 
+		// No key input.
+		if (currentVelocity > 0)
 			currentVelocity -= acceleration * Time.deltaTime;
-		else if (currentVelocity < 0) 
+		else if (currentVelocity < 0)
 			currentVelocity += acceleration * Time.deltaTime;
 
 	}
 
 
-	// Turn off engine if currentVelocity is too small. 
+	// Turn off engine if currentVelocity is too small.
 	if (Mathf.Abs(currentVelocity) <= 0.005)
 		currentVelocity = 0;
 
 	// Move Tank by currentVelocity
 	transform.Translate(Vector3(0, 0, currentVelocity * Time.deltaTime));
 
-	// Move Tracks by currentVelocity	 
+	// Move Tracks by currentVelocity
 	if (currentVelocity > 0) {
 		// Move forward
 		leftTrack.speed = currentVelocity;
@@ -74,10 +74,10 @@ function Update () {
 	}
 	else {
 		// No Move
-		leftTrack.GearStatus = 0;	
-		rightTrack.GearStatus = 0;		
+		leftTrack.GearStatus = 0;
+		rightTrack.GearStatus = 0;
 	}
-	
+
 	// Turn Tank
 	if (Input.GetKey (KeyCode.LeftArrow)) {
 		if (currentVelocity < 0) {
@@ -87,7 +87,7 @@ function Update () {
 			leftTrack.GearStatus = 1;
 			rightTrack.speed = rotationSpeed;
 			rightTrack.GearStatus = 2;
-			
+
 		} else {
 			// Turn left
 			transform.Rotate(Vector3(0, -rotationSpeed * Time.deltaTime, 0));
@@ -95,7 +95,7 @@ function Update () {
 			leftTrack.GearStatus = 2;
 			rightTrack.speed = rotationSpeed;
 			rightTrack.GearStatus = 1;
-			
+
 		}
 	}
 	if (Input.GetKey (KeyCode.RightArrow)) {
@@ -114,15 +114,15 @@ function Update () {
 			leftTrack.GearStatus = 1;
 			rightTrack.speed = rotationSpeed;
 			rightTrack.GearStatus = 2;
-			
+
 		}
 	}
-	
+
 	// Fire!
 	if (Input.GetButtonDown("Fire1")) {
 		// make fire effect.
 		Instantiate(fireEffect, spawnPoint.position, spawnPoint.rotation);
-		
+
 		// make ball
 		Instantiate(bulletObject, spawnPoint.position, spawnPoint.rotation);
 	}
