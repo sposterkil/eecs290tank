956ac248909b80bbab0e9260994fcbdebbb19b73
Sam Osterkil <pisketch@gmail.com>
2014-03-27 14:58:47 -0400

    Added Main Menu (Thanks Terri)

---
+++ Assets/Main Menu/MenuButtonText.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtonText: MonoBehaviour 
+{
+	public bool isQuit;
+	public bool isStart;
+
+	void OnMouseEnter()
+	{
+		renderer.material.color = Color.red;
+	}
+	
+
+	void OnMouseExit()
+	{
+		renderer.material.color = Color.white;
+	}
+	
+	void OnMouseDown() {
+		if(isStart) {
+			Application.LoadLevel(1);
+		}
+		else if(isQuit) {
+			Application.Quit();
+		}
+		else {
+		}
+	}
+}
